386153d705736a503c10415b1c28da1ee2c0df00
kristinasamuel <166210993+kristinasamuel@users.noreply.github.com>
2024-09-14 14:05:22 +0500

    style in countdown timer

--- components/count-down.tsx
+++ components/count-down.tsx
@@ -95,8 +95,6 @@ export default function Countdown() {
   return (
   <div className="flex items-center justify-center min-h-screen bg-gray-900">
   <div className="bg-red-600 rounded-lg p-4 md:p-8 w-full h-auto md:h-80  max-w-sm md:max-w-md">
-    
-
 
         <h1 className="text-4xl font-bold mb-4 text-gray-800 dark:text-gray-200 text-center">
           Countdown Timer
